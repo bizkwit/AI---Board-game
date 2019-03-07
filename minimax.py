39ffd12841406e96613c5ef5373b6deb961479a7
AndyZakk <AndyZakk@mail.ru>
2019-03-07 18:10:39 -0500

    Update minimax.py

--- minimax.py
+++ minimax.py
@@ -3,11 +3,11 @@ def e(board):
     for row in range(board.num_rows):
         for col in range(board.num_cols):
             if board.matrix[row][col].value == "Wo":
-                result += row * 10 + col + 1
+                result += col * 10 + row + 1
             elif board.matrix[row][col].value == "W*":
-                result += 3 * (row * 10 + col + 1)
+                result += 3 * (col * 10 + row + 1)
             elif board.matrix[row][col].value == "R*":
-                result -= 2 * (row * 10 + col + 1)
+                result -= 2 * (col * 10 + row + 1)
             elif board.matrix[row][col].value == "Ro":
-                result -= 1.5 * (row * 10 + col + 1)
+                result -= 1.5 * (col * 10 + row + 1)
     return result
