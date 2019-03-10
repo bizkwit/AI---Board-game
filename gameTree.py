3249aa91e036f725d02e2a44bc17b93bc7c280bb
AndyZakk <AndyZakk@mail.ru>
2019-03-09 23:31:38 -0500

    Update gameTree.py

--- gameTree.py
+++ gameTree.py
@@ -175,7 +175,7 @@ class GameTree:
             game.cards_count -= 1
         else:
             self.update_root(self.get_best_recycle_move(game, is_max))
-            game.last_card_played = self.root.placed_card
+        game.last_card_played = self.root.placed_card
 
     def print_tree(self):
         number_of_nodes = 1
